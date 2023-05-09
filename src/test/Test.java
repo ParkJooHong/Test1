4e737706d687ebb337e12d88686407533f52a02a
ParkJooHong <qkrwnghd512@gmail.com>
2023-05-09 17:17:09 +0900

    gd~~@

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,5 +3,8 @@ package test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(111);
+		System.out.println(121);
+		System.out.println(31);
+		System.out.println(111);
 	}
 }
